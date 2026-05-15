e2d81c45b76b0406ec2f3a188bec18ddee76e4ff
Pasha0923 <alexsai@i.ua>
2026-05-15 15:51:27 +0300

    refactor final

--- configuration/config.py
+++ configuration/config.py
@@ -1,7 +1,7 @@
 import torch
 BATCH_SIZE = 64
 LEARNING_RATE = 0.0001
-EPOCHS = 20
+EPOCHS = 12
 NUM_CLASSES = 10
 MODEL_PATH = "models/best_model_2.pth"
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
